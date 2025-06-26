d08cea6fa6b780d6788ffe88414740b626b77d37
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-06-26 16:23:36 -0500

    first-commit

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,38 @@
+import { title } from "process";
+
+export default {
+    name: 'blog',
+    type: 'document',
+    title: 'Blog',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title of blog post',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Slug of the blog post',
+            options: {
+                source: 'title',
+            },
+        },
+        {
+            name: 'titleImage',
+            type: 'image',
+            title: 'Title Image',
+        },
+        {
+            name: 'smallDescription',
+            type: 'text',
+            title: 'Small Description',
+        },
+        {
+            name: 'content',
+            type: 'array',
+            title: 'Content',
+            of: [{ type: 'block' }],
+        }
+    ]
+}
