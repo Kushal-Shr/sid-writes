ed8877110b0073791f3c34541163d348c76b5794
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-06-26 17:57:56 -0500

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ThemeProvider } from "@/components/themeProvider";
 
 const dm_sans = DM_Sans({
   subsets: ["latin"],
@@ -24,8 +25,10 @@ export default function RootLayout({
       <body
         className={`${dm_sans.variable} antialiased`}
       >
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <Header />  
         {children}
+      </ThemeProvider>
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import React from 'react'
 import Logo from './Logo'
 import Link from 'next/link'
 import { Button } from './ui/button'
 import { TiSocialLinkedin, TiSocialInstagramCircular,TiSocialFacebook, TiSocialTwitter } from "react-icons/ti";
 import { IoSunnySharp } from "react-icons/io5";
+import { FiMoon } from "react-icons/fi";
+import { useTheme } from 'next-themes';
 
 
 const Header = () => {
+
+    const { theme, setTheme } = useTheme();
+
+    const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => setMounted(true), [])
+
   return (
     <header className='w-full p-15 px-30 flex justify-between items-center border-1'>
       <Logo />
@@ -16,12 +26,21 @@ const Header = () => {
         <Link href = '/about'>About</Link>
         <Link href = '/blog'>Blog</Link>
         <Link href = '/contact'>Contact</Link>
-        <Button className='rounded-full '>
-            <IoSunnySharp className='text-2xl'/>
-        </Button>
+        {mounted && (
+          <Button
+            className='rounded-full'
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+            {theme === 'dark' ? (
+              <IoSunnySharp className='text-2xl' />
+            ) : (
+              <FiMoon className='text-2xl' />
+            )}
+          </Button>
+        )}
       </nav>
 
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center gap-4 p-6'>
         <Link href='#'>
             <TiSocialInstagramCircular className='text-3xl hover:scale-115 transition-all ease duration-200' />
         </Link>
